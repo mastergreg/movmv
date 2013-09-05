99c32a80a234faf7e03ac92f5f27c4b173fd8f28
Gregory Lyras <gregliras@gmail.com>
2013-09-05 19:11:04 +0300

    strip the whitespace

--- movmv.py
+++ movmv.py
@@ -4,7 +4,7 @@
 #* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 # File Name : movmv.py
 # Creation Date : 05-09-2013
-# Last Modified : Thu 05 Sep 2013 07:03:45 PM EEST
+# Last Modified : Thu 05 Sep 2013 07:10:48 PM EEST
 # Created By : Greg Liras <gregliras@gmail.com>
 #_._._._._._._._._._._._._._._._._._._._._.*/
 
@@ -16,11 +16,12 @@ from os import rename
 
 def main():
     fp = open(argv[1])
-    for name in fp:
+    for line in fp:
+        name = line.strip()
         guess = guessit.guess_movie_info(name, info = ['filename'])
         #print guess.nice_string()
         while True:
-            ans = raw_input("Do you want me to move {0} to {1}? (y/n)".format(name, guess["title"]))
+            ans = raw_input("Do you want me to move '{0}' to '{1}'? (y/n) ".format(name, guess["title"]))
             if ans == "y":
                 rename(name, guess["title"])
                 break
